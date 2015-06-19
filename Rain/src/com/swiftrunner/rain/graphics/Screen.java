689267bb22793cecd98edfd679fd28869ad98263
pschoeller <pschoeller001@gmail.com>
2015-06-18 21:31:22 -0500

    Added spritesheet class and some assets.

--- Rain/src/com/swiftrunner/rain/graphics/Screen.java
+++ Rain/src/com/swiftrunner/rain/graphics/Screen.java
@@ -7,7 +7,7 @@ public class Screen {
 	private int width, height;
 	public int[] pixels;
 	
-	public final int MAP_SIZE = 32;
+	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 	public int xOffset, yOffset;
