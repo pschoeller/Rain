0e6bd45da7a74a4c4f71b39cc265a4846780a6a9
pschoeller <pschoeller001@gmail.com>
2015-06-22 18:07:55 -0500

    Added Entity class.

---
+++ Rain/src/com/swiftrunner/rain/entity/Entity.java
@@ -0,0 +1,25 @@
+package com.swiftrunner.rain.entity;
+
+import java.util.Random;
+
+import com.swiftrunner.rain.graphics.Screen;
+import com.swiftrunner.rain.level.Level;
+
+
+public abstract class Entity {
+	
+	protected int x, y;
+	private boolean removed=false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	
+	public void update(){}
+	public void render(Screen screen){}
+	public void remove(){ removed = true; }
+	public boolean isRemoved(){ return removed; }
+	public int getX() { return x; }
+	public void setX(int x) { this.x = x; }
+	public int getY() { return y; }
+	public void setY(int y) { this.y = y; }
+}
