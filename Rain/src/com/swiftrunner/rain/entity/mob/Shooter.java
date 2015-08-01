b8047c12b5695a756ce2f2f885b710097220f05f
pschoeller <pschoeller001@gmail.com>
2015-07-31 20:30:36 -0500

    Adding fonts

--- Rain/src/com/swiftrunner/rain/entity/mob/Shooter.java
+++ Rain/src/com/swiftrunner/rain/entity/mob/Shooter.java
@@ -107,7 +107,7 @@ public class Shooter extends Mob{
 	
 	public void render(Screen screen){ 
 		sprite = animSprite.getSprite();
-		Debug.drawRect(screen, 17*16, 59*16, 60, 40, true);
+		Debug.drawRect(screen, 40, 40, 32, 32, false);
 		screen.renderMob(x, y, this, true); 
 	}
 }
---
+++ Rain/src/com/swiftrunner/rain/graphics/Font.java
@@ -0,0 +1,6 @@
+package com.swiftrunner.rain.graphics;
+
+public class Font {
+	
+//	/private static SpriteSheet font = new SpriteSheet("/fonts/arial.png", , );
+}
